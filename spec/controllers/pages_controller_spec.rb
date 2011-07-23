1f6621b370cbcda13e138b92b650a44cd859bd59
David Wilkins <wilkox@fastmail.fm>
2011-07-23 19:39:34 +1000

    added css, tightened up main view and spec

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,18 +3,22 @@ require 'spec_helper'
 describe PagesController do
 	render_views
 
+	before(:each) do
+		@base_title = "Ruby on Rails Tutorial Sample App"
+	end
+
 #test the home page
-  describe "GET 'home'" do
-    it "should be successful" do
-      get 'home'
-      response.should be_success
-    end
+	describe "GET 'home'" do
+		it "should be successful" do
+			get 'home'
+			response.should be_success
+		end
 
 	it "should have the right title" do
 		get 'home'
-		response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Home")
+		response.should have_selector("title", :content => @base_title + " | Home")
 	end
-  end
+	end
 
 #test the help page
 	describe "GET 'help'" do
@@ -24,21 +28,21 @@ describe PagesController do
 		end
 		it "should have the right title" do
 			get 'help'
-			response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Help")
+			response.should have_selector("title", :content => @base_title + " | Help")
 		end
 	end
 
 
-  describe "GET 'contact'" do
-    it "should be successful" do
-      get 'contact'
-      response.should be_success
-    end
+	describe "GET 'contact'" do
+		it "should be successful" do
+			get 'contact'
+			response.should be_success
+		end
 	it "should have the right title" do
 		get 'contact'
-		response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | Contact")
+		response.should have_selector("title", :content => @base_title + " | Contact")
 	end
-  end
+	end
 
 	describe "GET 'about'" do
 		it "should be successful" do
@@ -47,7 +51,7 @@ describe PagesController do
 		end
 	it "should have the right title" do
 		get 'about'
-		response.should have_selector("title", :content => "Ruby on Rails Tutorial Sample App | About")
+		response.should have_selector("title", :content => @base_title + " | About")
 	end
 	end
 end
