c484d82971c2f1c066d816a247378ced16595638
David Wilkins <wilkox@fastmail.fm>
2011-07-24 11:12:38 +1000

    Added tests for links in main view, fixed bad link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,12 +1,17 @@
 module ApplicationHelper
 
-	#generate appropriate page title
-	def title
-		base_title = "Ruby on Rails Tutorial Sample App"
-		if @title.nil?
-			base_title
-		else
-			"#{base_title} | #{@title}"
-		end
-	end
+  #generate appropriate page title
+  def title
+    base_title = "Ruby on Rails Tutorial Sample App"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
+
+  #set link to logo (logo helper)
+  def logo
+    logo = image_tag("logo.png", :alt => "Sample App", :class => "round")
+   end
 end
