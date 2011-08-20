7caa29595b92ceaf1e6cb79d6775ea4f67402e94
David Wilkins <david@wilkox.org>
2011-08-20 11:40:34 +1000

    Finished modelling microposts
    Micropost/user relationships complete, all specs passing successfully

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,6 +13,7 @@ class User < ActiveRecord::Base
   attr_accessor :password
   attr_accessible :name, :email, :password, :password_confirmation
 
+  has_many :microposts, :dependent => :destroy
   #automatically create the virtual password_confirmation attribute
   validates :password, :presence => true,
                        :confirmation => true,
