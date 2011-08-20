62fb5ee0371147ec03204ef17933c3ae20fd4452
David Wilkins <david@wilkox.org>
2011-08-20 12:48:05 +1000

    No more stupid error!

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,17 +16,13 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(:page => params[:page])
     @title = @user.name
   end 
 
   def new
     @title = "Sign up"
     @user = User.new
-  end
-
-  def show
-    @user = User.find(params[:id])
-    @title = @user.name
   end
 
   def create
